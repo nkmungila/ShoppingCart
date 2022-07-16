acd2b07ccc9d1045f94c20648ae2bbd464b6ca98
Naveena Krishna <nkmungila@gmail.com>
2022-07-16 09:28:25 +0530

    third commit

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,7 +1,16 @@
+/// <reference types = "Cypress" />
 describe('Home Page', () => {
-  it('passes', () => {
-    cy.visit('https://rahulshettyacademy.com/angularpractice/')
-    cy.get('input[name="name"]:nth-child(2)').type('Bob')
+  
+  before(()=>{
+    cy.fixture('example').then(function(data){
+      this.data = data
+      cy.log('Data',data)
+    })
+  })
+  it('passes', function() {
+    cy.visit('/')
+    cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+    cy.get('input[name="email"]').type('a@a.com')
     cy.get('#exampleFormControlSelect1').select('Female')
   })
 })

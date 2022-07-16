9e44c43fa62e5b111a0850b4f2570ef7d996ca67
Naveena Krishna <nkmungila@gmail.com>
2022-07-16 14:23:29 +0530

    changed to Fixture

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -3,14 +3,13 @@ describe('Home Page', () => {
   
   before(()=>{
     cy.fixture('example').then(function(data){
-      this.data = data
-      cy.log('Data',data)
+      this.data = data      
     })
   })
   it('passes', function() {
     cy.visit('/')
     cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
-    cy.get('input[name="email"]').type('a@a.com')
-    cy.get('#exampleFormControlSelect1').select('Female')
+    cy.get('input[name="email"]').type(this.data.email)
+    cy.get('#exampleFormControlSelect1').select(this.data.gender)
   })
 })

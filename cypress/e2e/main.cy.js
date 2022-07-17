181a63a0899de82f323527bcc25a0a72c36d7fd8
Naveena Krishna <nkmungila@gmail.com>
2022-07-17 18:57:47 +0530

    Check min lenght

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -11,5 +11,10 @@ describe('Home Page', () => {
     cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
     cy.get('input[name="email"]').type(this.data.email)
     cy.get('#exampleFormControlSelect1').select(this.data.gender)
+    cy.get('input[name="name"]:nth-child(2)').should('have.value',this.data.name)
+    cy.get('input[name="name"]').each(($e1,index, list) =>{
+      let textbox = $e1.val()
+      cy.log(textbox)
+    })
   })
 })
